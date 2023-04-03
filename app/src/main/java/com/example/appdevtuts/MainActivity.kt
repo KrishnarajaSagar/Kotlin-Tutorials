d4979fb681a633a9a9aef38eb05e7cd21ee2fe2f
KrishnarajaSagar <krsagar.t.s@gmail.com>
2023-04-04 01:34:16 +0530

    Display editText data on log

--- app/src/main/java/com/example/appdevtuts/MainActivity.kt
+++ app/src/main/java/com/example/appdevtuts/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.appdevtuts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnApply.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val birthDate = etBirthDate.text.toString()
+            val country = etCountry.text.toString()
+            Log.d("MainActivity",
+                "$firstName $lastName born on $birthDate at $country")
+        }
     }
 }
