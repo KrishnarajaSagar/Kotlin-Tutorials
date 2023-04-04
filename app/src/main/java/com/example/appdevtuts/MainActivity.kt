f5ab09c934f3852c03427a6b27ccb20b1b6dae06
KrishnarajaSagar <krsagar.t.s@gmail.com>
2023-04-05 01:26:15 +0530

    Toast - Default and custom

--- app/src/main/java/com/example/appdevtuts/MainActivity.kt
+++ app/src/main/java/com/example/appdevtuts/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.appdevtuts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +16,24 @@ class MainActivity : AppCompatActivity() {
             var a = etFirstNum.text.toString().toFloat()
             var b = etSecondNum.text.toString().toFloat()
             tvResult.text = (a+b).toString()
+            Toast.makeText(
+                this,
+                "${(a+b).toString()}",
+                Toast.LENGTH_LONG)
+                .show()
         }
         btnSubtract.setOnClickListener {
             var a = etFirstNum.text.toString().toFloat()
             var b = etSecondNum.text.toString().toFloat()
             tvResult.text = (a-b).toString()
+            Toast(this).apply {
+                duration = Toast.LENGTH_LONG
+                view = layoutInflater.inflate(
+                    R.layout.custom_toast,
+                    clToast
+                )
+                show()
+            }
         }
         btnMultiply.setOnClickListener {
             var a = etFirstNum.text.toString().toFloat()
