afd4eb22480ce0883986047d2bbe1fe8b03366b5
KrishnarajaSagar <krsagar.t.s@gmail.com>
2023-04-09 07:46:21 +0530

    Permissions

--- app/src/main/java/com/example/appdevtuts/MainActivity.kt
+++ app/src/main/java/com/example/appdevtuts/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.appdevtuts
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.custom_toast.*
 
@@ -13,9 +16,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnForward.setOnClickListener {
-            Intent(this,SecondActivity::class.java).also {
-                startActivity(it)
+        btnGrantPermission.setOnClickListener {
+            requestPermissions()
+        }
+    }
+    private fun hasReadStoragePermission() =
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasCoarseLocationPermission() =
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermissions() {
+        var permissionsToRequest = mutableListOf<String>()
+        if(!hasReadStoragePermission()) {
+            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if(!hasCoarseLocationPermission()) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+        if(permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(),0)
+        }
+        Log.d("PermissionsGranted","${hasReadStoragePermission()},${hasCoarseLocationPermission()}")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && requestCode == 0) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PermissionsGranted","${permissions[i]} granted")
+                }
             }
         }
     }
